cc1b1fc8379d23b96ed639d7086c58277a6ee9f4
SauravBudhathoki <budhathokisaurav354@gmail.com>
2025-04-06 01:01:59 +0545

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Number Guessing Game
+
+        Random random = new Random();
+        Scanner scanner =new Scanner(System.in);
+
+        int guess;
+        int attempts = 0;
+        int min =1;
+        int max =100;
+        int randomNumber =random.nextInt(min,max+1);
+
+        System.out.println("Number Guessing Game");
+        System.out.printf("Guess a number between %d-%d\n",min,max);
+
+        do{
+            System.out.print("Enter a guess:");
+            guess=scanner.nextInt();
+            attempts++;
+
+            if(guess<randomNumber){
+                System.out.println("Too Low! Try again.");
+            } else if (guess>randomNumber) {
+                System.out.println("Too High! Try Again.");
+            }else {
+                System.out.println("CORRECT! The number was "+ randomNumber);
+                System.out.println("Number of attempts is : "+ attempts);
+            }
+
+        }while(guess !=randomNumber);
+
+        //System.out.println("You have Won!");
+        scanner.close();
+
     }
 }
